3f7e2d9298986c49a45e82c31fd0b2d2d4726dea
erik-fasnacht <erik.fasnacht@particle.io>
2023-04-10 12:05:39 -0600

    added apache 2.0 license to files

--- mikroe_port/lib/mikroe-port-drv/src/drv_i2c_master.h
+++ mikroe_port/lib/mikroe-port-drv/src/drv_i2c_master.h
@@ -1,13 +1,18 @@
-/**
- ******************************************************************************
- * @file    drv_i2c_master.h
- * @authors Erik Fasnacht
- * @version V1.0.0
- * @date    22-Mar-2023
- * @brief   I2C Master driver layer to interface with MikroE CLICK libraries
- ******************************************************************************
- **/
-
+/*
+ * Copyright (c) 2023 Particle Industries, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
 #ifndef _DRV_I2C_MASTER_H_
 #define _DRV_I2C_MASTER_H_
